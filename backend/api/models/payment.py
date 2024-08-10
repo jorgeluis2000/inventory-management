3b5e203d914048f0e2adcee469c65311cfc9d5c3
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-08-10 12:46:43 -0500

    Chore: Cambios en los modelos, en la parte del decimal.

--- backend/api/models/payment.py
+++ backend/api/models/payment.py
@@ -2,7 +2,7 @@ from django.db import models
 from utils.constants import status
 
 class Payment(models.Model):
-    total_amount = models.DecimalField(max_digits=20, decimal_places=20)
+    total_amount = models.DecimalField(max_digits=100, decimal_places=20)
     status = models.PositiveSmallIntegerField(default=1, choices=status.STATUS_PAYMENT)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- backend/api/models/payment_detail.py
+++ backend/api/models/payment_detail.py
@@ -1,6 +1,7 @@
 from django.db import models 
 from api.models.payment import Payment
 from api.models.product import Product
+
 class PaymentDetail(models.Model):
     payment_id = models.ForeignKey(Payment, on_delete=models.CASCADE)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
